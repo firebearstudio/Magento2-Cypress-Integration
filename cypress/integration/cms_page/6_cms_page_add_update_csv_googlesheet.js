d752006ab0571092657e40b89934a7dea0796622
Mary <marystoikaaa@gmail.com>
2019-10-24 11:25:54 +0300

    new tests for cms page entity were added

---
+++ cypress/integration/cms_page/6_cms_page_add_update_csv_googlesheet.js
@@ -0,0 +1,65 @@
+context('Import Cms Page', () => {
+    it('add - csv - google sheet - new job', () => {
+        //login
+        cy.visit('http://import.com/admin')
+        cy.get('#username')
+            .type('admin').should('have.value', 'admin')
+        cy.get('#login')
+            .type('magento2').should('have.value', 'magento2')
+        cy.get('.actions').find('button').as('loginButton')
+        cy.get('@loginButton').click()
+
+        //go to import page
+        cy.get('.item-import-job').find('a').as('goToImportPageLink')
+        cy.get('@goToImportPageLink').click({force:true})
+
+        //go to new job page
+        cy.get('#add').as('addJobButton')
+        cy.get('@addJobButton').click()
+
+        //specify general section
+        cy.get('.general_is_active',{timeout: 60000}).find('.admin__actions-switch-label').as('generalIsActive')
+        cy.get('@generalIsActive').click()
+        cy.get('.general_title ').find('input')
+            .type('Cms Page Import - add - csv - google sheet')
+            .should('have.value', 'Cms Page Import - add - csv - google sheet')
+        cy.get('.general_reindex').find('.admin__actions-switch-label').as('generalReindex')
+        cy.get('@generalReindex').click()
+
+        //specify Import Settings section
+        cy.get('.fieldset_settings').find('.fieldset-wrapper-title').as('fieldsetSettings')
+        cy.get('@fieldsetSettings').click()
+        cy.get('.settings_entity').find('select').as('settingsEntity')
+        cy.get('@settingsEntity').select('cms_page');
+
+        //specify Import Behavior section
+        cy.get('.fieldset_behavior').find('.fieldset-wrapper-title').as('fieldsetBehaviour')
+        cy.get('.behavior_behavior').find('select').as('behaviorBehavior')
+        cy.get('@behaviorBehavior').select('append');
+
+        //specify Import Source section
+        cy.get('.import_source').find('select').as('importSource')
+        cy.get('@importSource').select('google');
+        cy.get('.google_file_path').find('input').as('googleFilePath')
+        cy.get('@googleFilePath')
+            .invoke('val', 'https://docs.google.com/spreadsheets/d/13FemIzzexF5koAdQYjbcKscqoCfXyknYWkQkbSZGPsk/edit#gid=1011856548')
+            .trigger('change')
+
+        //validate Import file
+        cy.get('.source_check_button').click()
+        cy.get('.fieldset_source').contains('File validated successfully',{timeout: 60000})
+
+        //save and run process
+        cy.get('#save_and_run').click({force:true})
+        cy.get('.run').click()
+
+        //check Import results
+        cy.get('#debug-run').contains('Entity cms_page',{timeout: 60000})
+        cy.get('#debug-run').contains('The import was successful.',{timeout: 60000})
+        cy.get('#debug-run').contains('REINDEX completed',{timeout: 60000})
+        cy.get('#debug-run').contains('This file is empty').should('not.exist')
+        cy.get('#debug-run').contains('Data validation failed').should('not.exist')
+        cy.get('#debug-run').contains('Invalid').should('not.exist')
+        cy.get('#debug-run').contains('Exception').should('not.exist')
+    })
+})
