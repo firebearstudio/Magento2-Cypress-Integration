8d4d54395efac8a2d7e146bce7f3838c5f768588
Mary <marystoikaaa@gmail.com>
2019-12-26 15:25:25 +0000

    [II-11]-new tests for page hierarchy entity

---
+++ cypress/integration/content/page hierarchy/2_import_delete_cms_page_export_file.js
@@ -0,0 +1,47 @@
+context('Import Page Hierarchy', () => {
+    it('delete - export file - csv - sftp - new job', () => {
+        //login
+        cy.loginToAdminPanel('ee')
+
+        //go to import page
+        cy.get('.item-import-job').find('a').as('goToImportPageLink')
+        cy.get('@goToImportPageLink').click({force:true})
+
+        //go to new job page
+        cy.get('#add').as('addJobButton')
+        cy.get('@addJobButton').click()
+
+        //specify general section
+        cy.generalImportSection('Page Hierarchy Import - export file - delete - csv - sftp')
+
+        //specify Import Settings section
+        cy.get('.fieldset_settings').find('.fieldset-wrapper-title').as('fieldsetSettings')
+        cy.get('@fieldsetSettings').click()
+        cy.get('.settings_entity').find('select').as('settingsEntity')
+        cy.get('@settingsEntity').select('content_hierarchy');
+
+        //specify Import Behavior section
+        cy.get('.fieldset_behavior').find('.fieldset-wrapper-title').as('fieldsetBehaviour')
+        cy.get('.behavior_behavior').find('select').as('behaviorBehavior')
+        cy.get('@behaviorBehavior').select('delete');
+
+        //specify Import Source section
+        cy.specifySftpSource('importSftp','/var/www/alex/files/test/export_page_hierarchy.csv')
+        
+        //validate Import file
+        cy.get('.source_check_button').click()
+        cy.get('.fieldset_source').contains('File validated successfully',{timeout: 60000})
+
+        //save and run process
+        cy.get('#save_and_run').click({force:true})
+        cy.get('.run').click()
+
+        //check Import results
+        cy.consoleImportResult('Entity content_hierarchy')
+
+        //check that page hierarchy were deleted
+        cy.get('#menu-magento-backend-content').find('.item-versionscms-page-hierarchy').find('a').as('goToPageHierarchyGrid')
+        cy.get('@goToPageHierarchyGrid').click({force:true})
+        cy.get('.cms-hierarchy-tree').find('#ext-gen21').should('not.exist')
+    })
+})
---
+++ cypress/integration/content/page hierarchy/4_cms_page_add_update_json_dropbox.js
@@ -0,0 +1,56 @@
+context('Import Page Hierarchy', () => {
+    it('add - ods - dropbox - new job', () => {
+        //login
+        cy.loginToAdminPanel('ee')
+
+        //go to import page
+        cy.get('.item-import-job').find('a').as('goToImportPageLink')
+        cy.get('@goToImportPageLink').click({force:true})
+
+        //go to new job page
+        cy.get('#add').as('addJobButton')
+        cy.get('@addJobButton').click()
+
+        //specify general section
+        cy.generalImportSection('Page Hierarchy Import - add - ods - dropbox')
+    
+        //specify Import Settings section
+        cy.get('.fieldset_settings').find('.fieldset-wrapper-title').as('fieldsetSettings')
+        cy.get('@fieldsetSettings').click()
+        cy.get('.settings_entity').find('select').as('settingsEntity')
+        cy.get('@settingsEntity').select('content_hierarchy');
+
+        //specify Import Behavior section
+        cy.get('.fieldset_behavior').find('.fieldset-wrapper-title').as('fieldsetBehaviour')
+        cy.get('.behavior_behavior').find('select').as('behaviorBehavior')
+        cy.get('@behaviorBehavior').select('append');
+
+        //specify Import Source section
+        cy.get('.type_file').find('select').as('importSourceType')
+        cy.get('@importSourceType').select('ods');
+        cy.dropboxSource('/page_hierarchy.ods','lOuV6o4bspAAAAAAAAAGGPTF7q7UYpyADa2pLknmU4gTh_yXjAAADOsMndY-TtyE')
+
+        //validate Import file
+        cy.get('.source_check_button').click()
+        cy.get('.fieldset_source').contains('File validated successfully',{timeout: 60000})
+
+        //save and run process
+        cy.get('#save_and_run').click({force:true})
+        cy.get('.run').click()
+
+        //check Import results
+        cy.consoleImportResult('Entity content_hierarchy')
+
+        //check that page hierarchy were added
+        cy.get('#menu-magento-backend-content').find('.item-versionscms-page-hierarchy').find('a').as('goToPageHierarchyGrid')
+        cy.get('@goToPageHierarchyGrid').click({force:true})
+        cy.get('.cms-hierarchy-tree').find('#ext-gen21').contains('First node',{timeout: 60000})
+        cy.get('.cms-hierarchy-tree').find('#ext-gen27').click()
+        cy.get('.x-tree-node-ct').find('#extdd-6').contains('Second node',{timeout: 60000})
+        cy.get('.x-tree-node-ct').find('#extdd-8').contains('Sample CMS page 2',{timeout: 60000})
+        cy.get('.x-tree-node-ct').find('#extdd-10').contains('Sample CMS page 1',{timeout: 60000})
+        cy.get('.x-tree-node-ct').find('#extdd-12').contains('Title New',{timeout: 60000})
+        cy.get('.x-tree-node-ct').find('#extdd-14').contains('Sample CMS page 3',{timeout: 60000})
+        cy.get('.x-tree-node-ct').find('#extdd-16').contains('Third node',{timeout: 60000})
+    })
+})
