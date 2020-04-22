757a66316eba05c13891c37cd8c86dcb5537e2ee
Mary <marystoikaaa@gmail.com>
2020-04-22 06:55:16 +0000

    [II-11]-new cart_price_rules' tests were added

---
+++ cypress/integration/marketing/cart_price_rule/01_cart_price_rules_add_update_csv_googlesheet.js
@@ -0,0 +1,48 @@
+context('Import Cart Price Rules', () => {
+    it('add update - csv - google sheet - new job', () => {
+        //login
+        cy.loginToAdminPanel('ce')
+
+        //go to import page
+        cy.get('.item-import-job').find('a').as('goToImportPageLink')
+        cy.get('@goToImportPageLink').click({force:true})
+
+        //go to new job page
+        cy.get('#add').as('addJobButton')
+        cy.get('@addJobButton').click()
+
+        //specify general section
+        cy.generalImportSection('Cart Price Rules Import - add update - csv - google sheet')
+
+        //specify Import Settings section
+        cy.get('.fieldset_settings').find('.fieldset-wrapper-title').as('fieldsetSettings')
+        cy.get('@fieldsetSettings').click()
+        cy.get('.settings_entity').find('select').as('settingsEntity')
+        cy.get('@settingsEntity').select('sales_rule');
+
+        //specify Import Behavior section
+        cy.get('.fieldset_behavior').find('.fieldset-wrapper-title').as('fieldsetBehaviour')
+        cy.get('.behavior_behavior').find('select').as('behaviorBehavior')
+        cy.get('@behaviorBehavior').select('append');
+
+        //specify Import Source section
+        cy.googlePathSource('https://docs.google.com/spreadsheets/d/13FemIzzexF5koAdQYjbcKscqoCfXyknYWkQkbSZGPsk/edit#gid=73091490')
+
+        //validate Import file
+        cy.get('.source_check_button').click()
+        cy.get('.fieldset_source').contains('File validated successfully',{timeout: 60000})
+
+        //save and run process
+        cy.get('#save_and_run').click({force:true})
+        cy.get('.run').click()
+
+        //check Import results
+        cy.consoleImportResult('Entity cart_price_rule')
+
+        //сheck that cart price rules were added
+        cy.get('#menu-magento-backend-marketing').find('.item-promo-quote').find('a').as('goToCartPriceRuleGrid')
+        cy.get('@goToCartPriceRuleGrid').click({force:true})
+        cy.get('tbody').find('.col-name').contains('Test 10% off',{timeout: 60000})
+        cy.get('tbody').find('.col-name').contains('Test $15 off',{timeout: 60000})
+    })
+})
---
+++ cypress/integration/marketing/cart_price_rule/08_export_filter_cart_price_rule_csv_sftp.js
@@ -0,0 +1,64 @@
+context('Export Cart Price Rule', () => {
+    it('csv - sftp - new job', () => {
+        //login
+        cy.loginToAdminPanel('ce')
+
+        //go to import page
+        cy.get('.item-export-job').find('a').as('goToExportPageLink')
+        cy.get('@goToExportPageLink').click({force:true})
+
+        //go to new job page
+        cy.get('#add').as('addJobButton')
+        cy.get('@addJobButton').click()
+
+        //specify general section
+        cy.get('.general_is_active',{timeout: 60000}).find('.admin__actions-switch-label').as('generalIsActive')
+        cy.get('@generalIsActive').click()
+        cy.get('.general_title ').find('input')
+            .type('Cart Price Rule Export - csv - sftp')
+            .should('have.value', 'Cart Price Rule Export - csv - sftp')
+
+        //specify Export Settings section
+        cy.get('.settings_entity').find('select').as('settingsEntity')
+        cy.get('@settingsEntity').select('sales_rule')
+
+        //specify Export Behavior section
+        cy.get('.behavior_behavior_field_file_format').find('select').as('fileFormat')
+        cy.get('@fileFormat').select('csv');
+
+        //specify Import Source section
+        cy.specifySftpSource('exportSftp','/var/www/alex/files/test/export_cart_price_rule.csv')
+
+        //check ftp connection
+        cy.get('.source_check_button').click()
+        cy.get('.fieldset_source').contains('Success! Your connection is ready!',{timeout: 60000})
+
+        //filter
+        cy.get('.source_filter_map_rows').find('tfoot').as('tfoot')
+        cy.get('@tfoot').find('.addButton').click({force:true})
+        cy.get('.record-1').find('.source_filter_map_source_filter_field').find('select').as('sourceDataExport')
+        cy.get('@sourceDataExport').select('rule_id')
+        cy.get('.record-1').find('.source_filter_map_source_filter_filter').find('[name="source_filter_map[text][source_filter_filter]"]')
+            .type('5')
+            .should('have.value', '5')
+        cy.get('@tfoot').find('.addButton').click({force:true})
+        cy.get('.record-2').find('.source_filter_map_source_filter_field').find('select').as('sourceDataExport')
+        cy.get('@sourceDataExport').select('name')
+        cy.get('.record-2').find('.source_filter_map_source_filter_filter').find('[name="source_filter_map[text][source_filter_filter]"]')
+            .type('10% off')
+            .should('have.value', '10% off')
+        cy.get('@tfoot').find('.addButton').click({force:true})
+        cy.get('.record-3').find('.source_filter_map_source_filter_field').find('select').as('sourceDataExport')
+        cy.get('@sourceDataExport').select('code')
+        cy.get('.record-3').find('.source_filter_map_source_filter_filter').find('[name="source_filter_map[text][source_filter_filter]"]')
+            .type('test10%off')
+            .should('have.value', 'test10%off')
+
+        //save and run process
+        cy.get('#save_and_run').click({force:true})
+        cy.get('.run').click()
+
+        //check Export results
+        cy.consoleExportResult('Entity cart_price_rule')
+    })
+})
