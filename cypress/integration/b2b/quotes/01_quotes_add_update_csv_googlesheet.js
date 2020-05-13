0c0105b19167a395632cdf70745a5b5e687c45a6
Mary <marystoikaaa@gmail.com>
2020-05-13 15:08:09 +0000

    [II-11]-new tests for quote entity were added

---
+++ cypress/integration/b2b/quotes/01_quotes_add_update_csv_googlesheet.js
@@ -0,0 +1,49 @@
+context('Import Quotes', () => {
+    it('add update - csv - google sheet - new job', () => {
+        //login
+        cy.loginToAdminPanel('ee')
+
+        //go to import page
+        cy.get('.item-import-job').find('a').as('goToImportPageLink')
+        cy.get('@goToImportPageLink').click({force:true})
+
+        //go to new job page
+        cy.get('#add').as('addJobButton')
+        cy.get('@addJobButton').click()
+
+        //specify general section
+        cy.generalImportSection('Quotes Import - add update - csv - google sheet')
+
+        //specify Import Settings section
+        cy.get('.fieldset_settings').find('.fieldset-wrapper-title').as('fieldsetSettings')
+        cy.get('@fieldsetSettings').click()
+        cy.get('.settings_entity').find('select').as('settingsEntity')
+        cy.get('@settingsEntity').select('quote');
+
+        //specify Import Behavior section
+        cy.get('.fieldset_behavior').find('.fieldset-wrapper-title').as('fieldsetBehaviour')
+        cy.get('.behavior_behavior').find('select').as('behaviorBehavior')
+        cy.get('@behaviorBehavior').select('add_update');
+
+        //specify Import Source section
+        cy.googlePathSource('https://docs.google.com/spreadsheets/d/1ASaPIdt8RrZfIP3f1ZmSPG9CQzAYMTNdfzQEhe38bPE/edit#gid=1840487190')
+
+        //validate Import file
+        cy.get('.source_check_button').click()
+        cy.get('.fieldset_source').contains('File validated successfully',{timeout: 60000})
+
+        //save and run process
+        cy.get('#save_and_run').click({force:true})
+        cy.get('.run').click()
+
+        //check Import results
+        cy.consoleImportResult('Entity quote')
+
+        //check that  Quotes were added
+        cy.get('#menu-magento-sales-sales').find('.item-quote-index').find('a').as('goToQuotesGrid')
+        cy.get('@goToQuotesGrid').click({force:true})
+        cy.get('[data-bind="text: $col.getLabel($row())"]').contains('Test quote for Doe')
+        cy.get('[data-bind="text: $col.getLabel($row())"]').contains('Test quote for Steve')
+        cy.get('[data-bind="text: $col.getLabel($row())"]').contains('Test quote for John')
+    })
+})
---
+++ cypress/integration/b2b/quotes/10_quotes_replace_csv_sftp.js
@@ -0,0 +1,47 @@
+context('Import quotes', () => {
+    it('quote - csv - file - new job', () => {
+        //login
+        cy.loginToAdminPanel('ee')
+
+        //go to import page
+        cy.get('.item-import-job').find('a').as('goToImportPageLink')
+        cy.get('@goToImportPageLink').click({force:true})
+
+        //go to new job page
+        cy.get('#add').as('addJobButton')
+        cy.get('@addJobButton').click()
+
+        //specify general section
+        cy.generalImportSection('quotes Import - replace - csv - file')
+
+        //specify Import Settings section
+        cy.get('.fieldset_settings').find('.fieldset-wrapper-title').as('fieldsetSettings')
+        cy.get('@fieldsetSettings').click()
+        cy.get('.settings_entity').find('select').as('settingsEntity')
+        cy.get('@settingsEntity').select('quote');
+
+        //specify Import Behavior section
+        cy.get('.fieldset_behavior').find('.fieldset-wrapper-title').as('fieldsetBehaviour')
+        cy.get('.behavior_behavior').find('select').as('behaviorBehavior')
+        cy.get('@behaviorBehavior').select('replace');
+
+        //specify Import Source section
+        cy.fileSource('pub/media/importexport/test/b2b_quote_replace.csv')
+
+        //validate Import file
+        cy.get('.source_check_button').click()
+        cy.get('.fieldset_source').contains('File validated successfully',{timeout: 60000})
+
+        //save and run process
+        cy.get('#save_and_run').click({force:true})
+        cy.get('.run').click()
+
+        //check Import results
+        cy.consoleImportResult('Entity quote')
+
+        //check that quotess have only Public type
+        cy.get('#menu-magento-sales-sales').find('.item-quote-index').find('a').as('goToQuotesGrid')
+        cy.get('@goToQuotesGrid').click({force:true})
+        cy.get('[data-bind="text: $col.getLabel($row())"]').contains('Google')
+    })
+})
