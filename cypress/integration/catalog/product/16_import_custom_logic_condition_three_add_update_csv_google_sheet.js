5b5e89d188625a51e1e9a43f573aba12a5f94f83
Mary <marystoikaaa@gmail.com>
2020-06-12 14:27:51 +0000

    custom logic tests were added

---
+++ cypress/integration/catalog/product/16_import_custom_logic_condition_three_add_update_csv_google_sheet.js
@@ -0,0 +1,64 @@
+
+context('Import Products', () => {
+    it('custom logic - condition three - add update -  csv - google sheet - new job', () => {
+        //login
+        cy.loginToAdminPanel('ee')
+
+        //go to import page
+        cy.get('.item-import-job').find('a').as('goToImportPageLink')
+        cy.get('@goToImportPageLink').click({force:true})
+
+        //go to new job page
+        cy.get('#add').as('addJobButton')
+        cy.get('@addJobButton').click()
+
+        //specify general section
+        cy.generalImportSection('Product Import - custom logic - condition three - add update - csv - google sheet')
+
+        //specify Import Settings section
+        cy.get('.fieldset_settings').find('.fieldset-wrapper-title').as('fieldsetSettings')
+        cy.get('@fieldsetSettings').click()
+        cy.get('.settings_entity').find('select').as('settingsEntity')
+        cy.get('@settingsEntity').select('catalog_product');
+
+        //specify Import Behavior section
+        cy.get('.fieldset_behavior').find('.fieldset-wrapper-title').as('fieldsetBehaviour')
+        cy.get('.behavior_behavior').find('select').as('behaviorBehavior')
+        cy.get('@behaviorBehavior').select('append');
+
+        //specify Import Source section
+        cy.googlePathSource('https://docs.google.com/spreadsheets/d/1dTZUJ96Y9TpKCYpNpqa2gvQ_55TPtaj7rI-dRXXqw_A/edit#gid=802580759')
+
+        //validate Import file
+        cy.get('.source_check_button').click()
+        cy.get('.fieldset_source').contains('File validated successfully',{timeout: 60000})
+
+        //set custom logic options
+        cy.get('.configurable_configurable_switch').find('.admin__actions-switch-label').as('customicLogic')
+        cy.get('@customicLogic').click()
+        cy.get('.configurable_configurable_create').find('.admin__actions-switch-label').as('configProductEnabled')
+        cy.get('@configProductEnabled').click()
+        cy.get('.configurable_configurable_type').find('select').as('configType')
+        cy.get('@configType').select('part_down');
+        cy.get('.configurable_configurable_field').find('select').as('configField')
+        cy.get('@configField').select('sku');
+        cy.get('.configurable_configurable_part').find('input')
+           .type('-')
+           .should('have.value', '-')
+        cy.get('.configurable_variations_rows').find('.addButton').as('configVariation')
+        cy.get('@configVariation').click()
+        cy.get('.configurable_variations_configurable_variations_attributes').find('select').as('configVariationAttributeFirst')
+        cy.get('@configVariationAttributeFirst').select('color');
+        cy.get('.configurable_variations_rows').find('.addButton').as('configVariation')
+        cy.get('@configVariation').click()
+        cy.get('.configurable_variations_rows').find('.record-2').find('select').as('configVariationAttributeSecond')
+        cy.get('@configVariationAttributeSecond').select('size');
+
+        //save and run process
+        cy.get('#save_and_run').click({force:true})
+        cy.get('.run').click()
+
+        //check Import results
+        cy.consoleImportResult('Entity products')
+    })
+})
