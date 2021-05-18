bf0084c9edc9d063a022bc27f9af0323407beae5
Maria Stoika <marystoikaaa@gmail.com>
2021-05-18 15:14:21 +0300

    Tests for validation strategies were added

---
+++ cypress/integration/01-catalog/1-product/2-features/6-other/08_import_products_skip_errors.js
@@ -0,0 +1,56 @@
+
+context('Import Products Skip Errors', () => {
+    it('import products - skip errors', () => {
+        //login
+        cy.loginToAdminPanel('ee')
+
+        //go to import page
+        cy.get('.item-import-job').find('a').as('goToImportPageLink')
+        cy.get('@goToImportPageLink').click({force:true})
+
+        //go to new job page
+        cy.get('#add').as('addJobButton')
+        cy.get('@addJobButton').click()
+
+        //specify general section
+        cy.generalImportSectionWithoutReIndex('Product Import - skip errors')
+
+        //specify Import Settings section
+        cy.get('.fieldset_settings').find('.fieldset-wrapper-title').as('fieldsetSettings')
+        cy.get('@fieldsetSettings').click()
+        cy.get('.settings_entity').find('select').as('settingsEntity')
+        cy.get('@settingsEntity').select('catalog_product');
+
+        //specify Import Behavior section
+        cy.get('.fieldset_behavior').find('.fieldset-wrapper-title').as('fieldsetBehaviour')
+        cy.get('.behavior_behavior').find('select').as('behaviorBehavior')
+        cy.get('@behaviorBehavior').select('append');
+        cy.get('[data-index="validation_strategy"]').find('select').as('fieldValedationStrategy')
+        cy.get('@fieldValedationStrategy').select('validation-skip-errors')
+
+        //specify Import Source section
+        cy.fileSource('pub/media/importexport/test/products_for_skip_errors.csv')
+
+        //validate Import file
+        cy.get('.source_check_button').click()
+        cy.get('.fieldset_source').contains('File validated successfully',{timeout: 60000})
+
+        //save and run process
+        cy.get('#save_and_run').click({force:true})
+        cy.get('.run').click()
+
+        //check Import results
+        cy.consoleImportResultWithoutReIndex('Entity products')
+        cy.get('#debug-run').contains('Please make sure attribute "sku" is not empty.',{timeout: 120000})
+        
+        //check that products were created
+        cy.get('#menu-magento-catalog-catalog').find('.item-catalog-products').find('a').as('goToProductsGrid')
+        cy.get('@goToProductsGrid').click({force:true})
+        cy.get('[data-bind="collapsible: {openClass: false, closeOnOuter: false}"]',{timeout: 60000}).find('button').as('filtersButton')
+        cy.get('@filtersButton').click({force:true})
+        cy.get('[name="sku"]').invoke('val', 'skip-error-test').trigger('change',{force:true})
+        cy.get('[data-bind="i18n: \'Apply Filters\'"]',{timeout: 60000}).as('applyFiltersButton')
+        cy.get('@applyFiltersButton').click({force:true})
+        cy.get('.admin__data-grid-outer-wrap').contains('3 records found',{timeout: 60000})
+    })
+})
---
+++ cypress/integration/01-catalog/1-product/2-features/6-other/10_import_products_stop_error_filed.js
@@ -0,0 +1,49 @@
+
+context('Import Products Stop on Error - Failed', () => {
+    it('import products - stop on Error - Failed', () => {
+        //login
+        cy.loginToAdminPanel('ee')
+
+        //go to import page
+        cy.get('.item-import-job').find('a').as('goToImportPageLink')
+        cy.get('@goToImportPageLink').click({force:true})
+
+        //go to new job page
+        cy.get('#add').as('addJobButton')
+        cy.get('@addJobButton').click()
+
+        //specify general section
+        cy.generalImportSectionWithoutReIndex('Product Import - stop on error - failed')
+
+        //specify Import Settings section
+        cy.get('.fieldset_settings').find('.fieldset-wrapper-title').as('fieldsetSettings')
+        cy.get('@fieldsetSettings').click()
+        cy.get('.settings_entity').find('select').as('settingsEntity')
+        cy.get('@settingsEntity').select('catalog_product');
+
+        //specify Import Behavior section
+        cy.get('.fieldset_behavior').find('.fieldset-wrapper-title').as('fieldsetBehaviour')
+        cy.get('.behavior_behavior').find('select').as('behaviorBehavior')
+        cy.get('@behaviorBehavior').select('append');
+        cy.get('[data-index="validation_strategy"]').find('select').as('fieldValedationStrategy')
+        cy.get('@fieldValedationStrategy').select('validation-stop-on-errors')
+        cy.get('[data-index="allowed_error_count"]').find('input').as('allowedErrorCount')
+        cy.get('@allowedErrorCount').invoke('val', '2').trigger('change',{force:true})
+
+        //specify Import Source section
+        cy.fileSource('pub/media/importexport/test/products_for_stop_erros.csv')
+
+        //validate Import file
+        cy.get('.source_check_button').click()
+        cy.get('.fieldset_source').contains('File validated successfully',{timeout: 60000})
+
+        //save and run process
+        cy.get('#save_and_run').click({force:true})
+        cy.get('.run').click()
+
+        //check Import results
+        cy.get('#debug-run').contains('Please make sure attribute "sku" is not empty.',{timeout: 120000})
+        cy.get('#debug-run').contains('Data validation failed. Please fix the following errors and upload the file again.',{timeout: 120000})
+        cy.get('#debug-run').contains('Data validation is failed. Please fix errors and re-upload the file',{timeout: 120000})
+    })
+})
