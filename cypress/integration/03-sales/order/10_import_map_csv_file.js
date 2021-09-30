03fadc4b041b45baf42fe346d0923b169ad54e75
Maria Stoika <marystoikaaa@gmail.com>
2021-09-30 13:49:30 +0300

    mapping tests were modified for the order entity

--- cypress/integration/03-sales/order/10_import_map_csv_file.js
+++ cypress/integration/03-sales/order/10_import_map_csv_file.js
@@ -34,27 +34,9 @@ context('Import Orders Mapping Empty Rows Xml 10', () => {
         cy.get('.fieldset_source').contains('File validated successfully',{timeout: 60000})
 
         //map attributes
-        cy.get('tfoot').find('.addButton').as('tfoot')
-        cy.get('@tfoot').click({force:true})
-        cy.get('.record-1').find('.source_data_map_source_data_system').find('select').as('sourceDataSystem')
-        cy.get('@sourceDataSystem').select('entity_id');
-        cy.get('.record-1').find('.source_data_map_source_data_import').find('select').as('sourceDataImport')
-        cy.get('@sourceDataImport').select('entity_id_map');
-
-        cy.get('tfoot').find('.addButton').as('tfoot')
-        cy.get('@tfoot').click({force:true})
-        cy.get('.record-2').find('.source_data_map_source_data_system').find('select').as('sourceDataSystem')
-        cy.get('@sourceDataSystem').select('increment_id');
-        cy.get('.record-2').find('.source_data_map_source_data_import').find('select').as('sourceDataImport')
-        cy.get('@sourceDataImport').select('increment_id_map');
-
-        cy.get('tfoot').find('.addButton').as('tfoot')
-        cy.get('@tfoot').click({force:true})
-        cy.get('.record-3').find('.source_data_map_source_data_system').find('select').as('sourceDataSystem')
-        cy.get('@sourceDataSystem').select('status');
-        cy.get('.record-3').find('.source_data_map_source_data_import').find('select').as('sourceDataImport')
-        cy.get('@sourceDataImport').select('status_map')
-        cy.get('.source_data_map_source_data_replace').find('input')
+        cy.addMappingRowImport('.record-1','entity_id','entity_id_map')
+        cy.addMappingRowImport('.record-2','increment_id','increment_id_map')
+        cy.addMappingRowImport('.record-3','status','status_map')
 
         //save and run process
         cy.get('#save_and_run').click({force:true})
