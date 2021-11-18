f9d8fbffa5fddcef1b6000c93ba308817f7c571a
Maria Stoika <marystoikaaa@gmail.com>
2021-11-18 19:06:47 +0200

    new tests were added for checking custom logic with copy values to configurable parent and import parent configs own categories

---
+++ cypress/integration/01-catalog/1-product/2-features/1-custom logic/07_import_attributes_for_checking_copy_to_parent.js
@@ -0,0 +1,45 @@
+context('Import Attributes for Custom logic Copy to Parent 7', () => {
+    it(' atttributes - custom logic - copy to parent', () => {
+        //login
+        cy.loginToAdminPanel('ee')
+
+        //go to import page
+        cy.get('.item-import-job').find('a').as('goToImportPageLink')
+        cy.get('@goToImportPageLink').click({force:true})
+
+        //go to new job page
+        cy.get('#add').as('addJobButton')
+        cy.get('@addJobButton').click({force:true})
+
+        //specify general section
+        cy.generalImportSectionWithoutReIndex('Attributes Import - custom logic copy to parent')
+
+        //specify Import Settings section
+        cy.get('.fieldset_settings').find('.fieldset-wrapper-title').as('fieldsetSettings')
+        cy.get('@fieldsetSettings').click({force:true})
+        cy.get('.settings_entity').find('select').as('settingsEntity')
+        cy.get('@settingsEntity').select('attribute',{force:true});
+
+        //specify Import Behavior section
+        cy.get('.fieldset_behavior').find('.fieldset-wrapper-title').as('fieldsetBehaviour')
+        cy.get('.behavior_behavior').find('select').as('behaviorBehavior')
+        cy.get('@behaviorBehavior').select('append',{force:true});
+
+        //specify Import Source section
+        cy.get('.type_file').find('select').as('importSourceType')
+        cy.get('@importSourceType').select('csv',{force:true});
+        cy.get('[data-index="import_source"]').find('select').select('file',{force:true})
+        cy.fileSource('pub/media/importexport/test/attributes_for_custom_logic.csv',{force:true})
+
+        //validate Import file
+        cy.get('.source_check_button').click({force:true})
+        cy.get('.fieldset_source').contains('File validated successfully',{timeout: 60000})
+
+        //save and run process
+        cy.get('#save_and_run').click({force:true})
+        cy.get('.run').click()
+
+        //check Import results
+        cy.consoleImportResultWithoutReIndex('Entity attribute')
+    })
+})
