4a2215928e3698ea0f5167f8da6d7ae048f8a631
Maria Stoika <marystoikaaa@gmail.com>
2021-11-24 15:55:22 +0200

    google drive tests were added for customer main,customer address,customers and addresses entities

---
+++ cypress/integration/12-google drive/13_export_customer_main_google_drive_spec.js
@@ -0,0 +1,48 @@
+
+context('Export Customer Main Google Drive 13',() => {
+    it('export - customer main - google drive - new job', () => {
+        //login
+        cy.loginToAdminPanel('ee')
+
+        //go to import page
+        cy.get('.item-export-job').find('a').as('goToExportPageLink')
+        cy.get('@goToExportPageLink').click({force:true})
+
+        //go to new job page
+        cy.get('#add').as('addJobButton')
+        cy.get('@addJobButton').click({force:true})
+
+        //specify general section
+        cy.get('.general_is_active',{timeout: 60000}).find('.admin__actions-switch-label').as('generalIsActive')
+        cy.get('@generalIsActive').click({force:true})
+        cy.get('.general_title ').find('input')
+            .type('Customer Main Export - google drive')
+            .should('have.value', 'Customer Main Export - google drive')
+
+        //specify Export Settings section
+        cy.get('.settings_entity').find('select').as('settingsEntity')
+        cy.get('@settingsEntity').select('customer',{force:true})
+
+        //specify Export Behavior section
+        cy.get('.behavior_behavior_field_file_format').find('select').as('fileFormat')
+        cy.get('@fileFormat').select('xlsx',{force:true});
+
+        //specify Import Source section
+        cy.get('[data-index="export_source_entity"]').find('select').select('googledrive')
+        cy.get('[data-index="export_source_googledrive_file_path"]').find('input')
+            .type('new/customer-main-google-drive-test.xlsx',{force:true})
+            .should('have.value','new/customer-main-google-drive-test.xlsx')
+
+        //upload Google Drive key
+        const filepath = 'files/new1-268508-0814a8f755d1.json'
+        cy.get('.export_source_googledrive_signing_key_file').find('input[type="file"]').attachFile(filepath)
+        cy.get('.file-uploader-filename',{timeout: 10000}).contains('new1-268508-0814a8f755d1.json')
+
+        //save and run process
+        cy.get('#save_and_run').click({force:true})
+        cy.get('.run').click()
+
+        //check Export results
+        cy.consoleExportResult('Entity customer_main')
+    })
+})
