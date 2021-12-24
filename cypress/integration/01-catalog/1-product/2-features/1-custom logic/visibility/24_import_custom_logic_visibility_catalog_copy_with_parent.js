a83e2286e9b27ccb46d1703440e889b8fa94d85c
Maria Stoika <marystoikaaa@gmail.com>
2021-12-24 16:55:18 +0200

    custom logic tests were added for the importing visibility values

---
+++ cypress/integration/01-catalog/1-product/2-features/1-custom logic/visibility/24_import_custom_logic_visibility_catalog_copy_with_parent.js
@@ -0,0 +1,106 @@
+
+context('Import Products Custom logic Visibility Catalog Copy With Parent 24',{ retries: 0 }, () => {
+    it('custom logic - visibility - Catalog - Copy - With parent', () => {
+        //login
+        cy.loginToAdminPanel('ee')
+
+        //go to import page
+        cy.get('.item-import-job').find('a').as('goToImportPageLink')
+        cy.get('@goToImportPageLink').click({force:true})
+
+        //go to new job page
+        cy.get('#add').as('addJobButton')
+        cy.get('@addJobButton').click()
+
+        //specify general section
+        cy.generalImportSectionWithoutReIndex('Product Import - custom logic - copy - visibility - catalog - WITH parent')
+
+        //specify Import Settings section
+        cy.get('.fieldset_settings').find('.fieldset-wrapper-title').as('fieldsetSettings')
+        cy.get('@fieldsetSettings').click()
+        cy.get('.settings_entity').find('select').as('settingsEntity')
+        cy.get('@settingsEntity').select('catalog_product',{force:true});
+
+        //specify Import Behavior section
+        cy.get('.fieldset_behavior').find('.fieldset-wrapper-title').as('fieldsetBehaviour')
+        cy.get('.behavior_behavior').find('select').as('behaviorBehavior')
+        cy.get('@behaviorBehavior').select('append',{force:true});
+
+        //specify Import Source section
+        cy.fileSource('pub/media/importexport/test/products_custom_logic_visibility_catalog_with_parent.csv')
+
+        //validate Import file
+        cy.get('.source_check_button').click()
+        cy.get('.fieldset_source').contains('File validated successfully',{timeout: 60000})
+
+        //set custom logic options
+        cy.get('.configurable_configurable_switch').find('.admin__actions-switch-label').as('customicLogic')
+        cy.get('@customicLogic').click()
+        cy.get('.configurable_configurable_type').find('select').as('configType')
+        cy.get('@configType').select('field');
+        cy.get('.configurable_configurable_field').find('select').as('configField')
+        cy.get('@configField').select('group');
+        cy.get('.configurable_variations_rows').find('.addButton').as('configVariation')
+        cy.get('@configVariation').click()
+        cy.get('.configurable_variations_configurable_variations_attributes').find('select').as('configVariationAttributeFirst')
+        cy.get('@configVariationAttributeFirst').select('color');
+        cy.get('@configVariation').click()
+        cy.get('.configurable_variations_rows').find('.record-2').find('select').as('configVariationAttributeSecond')
+        cy.get('@configVariationAttributeSecond').select('size');
+
+        //set product attributes to copy value 
+        cy.get('[data-index="copy_simple_value"]').find('.addButton').as('configCopyAttributes')
+        cy.get('@configCopyAttributes').click()
+        cy.get('[name="copy_simple_value[0][copy_simple_value_attributes]"]').select('visibility');
+
+        //save and run process
+        cy.get('#save_and_run').click({force:true})
+        cy.get('.run').click()
+
+        //check Import results
+        cy.consoleImportResultWithoutReIndex('Entity catalog_product')
+
+        //check that products were created
+        cy.get('#menu-magento-catalog-catalog').find('.item-catalog-products').find('a').as('goToProductsGrid')
+        cy.get('@goToProductsGrid').click({force:true})
+        cy.get('[data-bind="collapsible: {openClass: false, closeOnOuter: false}"]',{timeout: 10000}).find('button').as('filtersButton')
+        cy.get('@filtersButton').click({force:true})
+        cy.get('[name="sku"]').invoke('val', 'visibility-six').trigger('change',{force:true})
+        cy.get('[data-bind="i18n: \'Apply Filters\'"]',{timeout: 10000}).as('applyFiltersButton')
+        cy.get('@applyFiltersButton').click({force:true})
+
+        cy.get('table',{timeout:40000}).contains('visibility-six-S-Gray').parentsUntil('tbody')
+            .within(function () {
+                cy.contains('Catalog')
+            })
+        cy.get('table',{timeout:40000}).contains('visibility-six-S-Green').parentsUntil('tbody')
+            .within(function () {
+                cy.contains('Catalog')
+            })
+            cy.get('table',{timeout:40000}).contains('visibility-six-S-Purple').parentsUntil('tbody')
+            .within(function () {
+                cy.contains('Catalog')
+            })
+            cy.get('table',{timeout:40000}).contains('visibility-six-M-Gray').parentsUntil('tbody')
+            .within(function () {
+                cy.contains('Catalog')
+            })
+            cy.get('table',{timeout:40000}).contains('visibility-six-M-Green').parentsUntil('tbody')
+            .within(function () {
+                cy.contains('Catalog')
+            })
+            cy.get('table',{timeout:40000}).contains('Configurable Product').parentsUntil('tbody')
+            .within(function () {
+                cy.contains('Catalog')
+            })
+
+        //delete 'visibility-six' products
+        cy.get('table').find('.data-grid-checkbox-cell-inner',{timeout: 60000}).click({multiple:true})
+        cy.get('.admin__data-grid-header-row').contains('Actions').as('actionsWithMultiple')
+        cy.get('@actionsWithMultiple').click({force:true})
+        cy.get('.action-menu-items').contains('Delete',{timeout: 60000})
+        cy.get('.action-menu-items').contains('Delete').click({force:true})
+        cy.get('.modal-footer').find('.action-accept').click({force:true}) 
+
+    })
+})
