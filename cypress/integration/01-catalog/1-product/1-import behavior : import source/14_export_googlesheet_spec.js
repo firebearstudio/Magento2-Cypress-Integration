69c3f4883c9b97924e1851bd09f2fd108f282895
Maria Stoika <marystoikaaa@gmail.com>
2021-12-29 18:12:29 +0200

    new test was added for checking export googlesheet

---
+++ cypress/integration/01-catalog/1-product/1-import behavior : import source/14_export_googlesheet_spec.js
@@ -0,0 +1,58 @@
+
+context('Export Products GoogleSheet 16',() => {
+    it('export - googlesheet - new job', () => {
+        //login
+        cy.loginToAdminPanel('ee')
+
+        //go to import page
+        cy.get('.item-export-job').find('a').as('goToExportPageLink')
+        cy.get('@goToExportPageLink').click({force:true})
+
+        //go to new job page
+        cy.get('#add').as('addJobButton')
+        cy.get('@addJobButton').click({force:true})
+
+        //specify general section
+        cy.get('.general_is_active',{timeout: 60000}).find('.admin__actions-switch-label').as('generalIsActive')
+        cy.get('@generalIsActive').click({force:true})
+        cy.get('.general_title ').find('input')
+            .type('Product Export - googlesheet')
+            .should('have.value', 'Product Export - googlesheet')
+
+        //specify Export Settings section
+        cy.get('.settings_entity').find('select').as('settingsEntity')
+        cy.get('@settingsEntity').select('catalog_product',{force:true})
+
+        //enable only admin values
+        cy.get('[data-index="only_admin"]').find('.admin__actions-switch-label').click()
+
+        //specify Export Behavior section
+        cy.get('.behavior_behavior_field_file_format').find('select').as('fileFormat')
+        cy.get('@fileFormat').select('google',{force:true});
+
+        //enable 'Export googlesheet' feature
+        cy.get('[name="export_source_google_spreadsheet_id"]').type('1mzKGoCba6-11DK1gYpMDBaes_s42lI1WR0MeT1VeCrU')
+        cy.get('[name="export_source_google_sheet_id"]').type('0')
+
+        //upload Google Drive key
+        const filepath = 'files/new1-268508-0814a8f755d1.json'
+        cy.get('.export_source_google_signing_key_file').find('input[type="file"]').attachFile(filepath)
+        cy.get('.file-uploader-filename',{timeout: 10000}).contains('new1-268508-0814a8f755d1.json')
+
+        //filter
+        cy.get('.source_filter_map_rows').find('tfoot').as('tfoot')
+        cy.get('@tfoot').find('.addButton').click({force:true})
+        cy.get('.record-1').find('[name="source_filter_field[value][0]"]').select('SKU')
+        cy.get('.record-1').find('.source_filter_map_source_filter_filter').find('[name="source_filter_map[text][source_filter_filter]"]').clear()
+        cy.get('.record-1').find('.source_filter_map_source_filter_filter').find('[name="source_filter_map[text][source_filter_filter]"]')
+            .type('tst')
+            .should('have.value', 'tst')
+
+        //save and run process
+        cy.get('#save_and_run').click({force:true})
+        cy.get('.run').click()
+
+        //check Export results
+        cy.consoleExportResult('Entity products')
+    })
+})
