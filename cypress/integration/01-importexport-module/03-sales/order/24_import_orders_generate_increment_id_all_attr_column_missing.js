8c812bdec4a7fca75187141b3032adc3a1224c58
Maria Stoika <marystoikaaa@gmail.com>
2023-03-02 15:58:51 +0200

    new tests for creation orders increment ids automatically were added

---
+++ cypress/integration/01-importexport-module/03-sales/order/24_import_orders_generate_increment_id_all_attr_column_missing.js
@@ -0,0 +1,47 @@
+
+context('Import Orders Generate Increment Id All Attr Column Missing 24', () => {
+    it('add update - generate - increment - id - all - attr - new job', () => {
+        //login
+        cy.loginToAdminPanel('ee')
+
+        //go to import page
+        cy.get('.item-import-job').find('a').as('goToImportPageLink')
+        cy.get('@goToImportPageLink').click({force:true})
+
+        //go to new job page
+        cy.get('#add').as('addJobButton')
+        cy.get('@addJobButton').click({force:true})
+
+        //specify general section
+        cy.generalImportSectionWithoutReIndex('Orders Import - generate - incrementId - all attr - column missing')
+
+        //specify Import Settings section
+        cy.get('.fieldset_settings').find('.fieldset-wrapper-title').as('fieldsetSettings')
+        cy.get('@fieldsetSettings').click({force:true})
+        cy.get('.settings_entity').find('select').as('settingsEntity')
+        cy.get('@settingsEntity').select('order',{force:true});
+
+        //enable generate increment id
+        cy.get('[data-index="use_auto_increment"]',{timeout: 60000}).find('.admin__actions-switch-label').as('generateIncrementId')
+        cy.get('@generateIncrementId').click({force:true})
+
+        //specify Import Behavior section
+        cy.get('.fieldset_behavior').find('.fieldset-wrapper-title').as('fieldsetBehaviour')
+        cy.get('.behavior_behavior').find('select').as('behaviorBehavior')
+        cy.get('@behaviorBehavior').select('add_update',{force:true});
+
+        //specify Import Source section
+        cy.fileSource('pub/media/importexport/test/orders-max-attr-for-generating-increment-id-missing.csv')
+
+        //validate Import file
+        cy.get('.source_check_button').click({force:true})
+        cy.get('.fieldset_source').contains('File validated successfully',{timeout: 60000})
+
+        //save and run process
+        cy.get('#save_and_run').click({force:true})
+        cy.get('.run').click()
+
+        //check Import results
+        cy.consoleImportResultWithoutReIndex('Entity order')
+    })
+})
