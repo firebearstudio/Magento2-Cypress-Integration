a1454073fd9df7829cc0ab8dca52b3a4ffa58954
Maria Stoika <marystoikaaa@gmail.com>
2023-03-10 19:14:02 +0200

    new tests were added for checking attributes import with attributes sets in one column

---
+++ cypress/integration/01-importexport-module/01-catalog/3-attribute/19_import_attributes_set_in_one_column_spec.js
@@ -0,0 +1,51 @@
+context('Import Attributes Set Is In One Column 19', () => {
+    it('attributes import - set is in one column - new job', () => {
+        //login
+        cy.loginToAdminPanel('ee')
+
+        //go to import page
+        cy.get('.item-import-job').find('a').as('goToImportPageLink')
+        cy.get('@goToImportPageLink').click({force:true})
+
+        //go to new job page
+        cy.get('#add').as('addJobButton')
+        cy.get('@addJobButton').click({force:true})
+
+        //specify general section
+        cy.generalImportSection('Attributes Import - set is in one column')
+
+        //specify Import Settings section
+        cy.get('.fieldset_settings').find('.fieldset-wrapper-title').as('fieldsetSettings')
+        cy.get('@fieldsetSettings').click()
+        cy.get('.settings_entity').find('select').as('settingsEntity')
+        cy.get('@settingsEntity').select('attribute',{force:true});
+
+        //specify Import Behavior section
+        cy.get('.fieldset_behavior').find('.fieldset-wrapper-title').as('fieldsetBehaviour')
+        cy.get('.behavior_behavior').find('select').as('behaviorBehavior')
+        cy.get('@behaviorBehavior').select('append',{force:true});
+
+        //specify Import Source section
+        cy.fileSource('pub/media/importexport/test/attributes_set_in_one_common_column.csv',{force:true})
+
+        //validate Import file
+        cy.get('.source_check_button').click({force:true})
+        cy.get('.fieldset_source').contains('File validated successfully',{timeout: 60000})
+
+        //save and run process
+        cy.get('#save_and_run').click({force:true})
+        cy.get('.run').click()
+
+        //check Import results
+        cy.consoleImportResult('Entity attribute')
+
+        //check that an attribute was imported
+        cy.get('#menu-magento-backend-stores').find('.item-catalog-attributes-attributes').find('a').as('goToAttributes')
+        cy.get('@goToAttributes').click({force:true})
+        cy.get('#attributeGrid_filter_attribute_code')
+            .type('test_qa_attribute',{force: true})
+            .should('have.value', 'test_qa_attribute')
+            .type('{enter}')
+        cy.get('#attributeGrid').contains('1 records found')
+    })
+})
