a317ebfdd815fa62bd9c3ccc4be173c6a8ecdaa6
Maria Stoika <marystoikaaa@gmail.com>
2023-05-26 18:54:09 +0300

    products weight factory tests were added

---
+++ cypress/integration/01-importexport-module/01-catalog/1-product/2-features/02-import-settings/13-weight-factory/02_import_product-weight-factory-feature-empty-value_spec.js
@@ -0,0 +1,59 @@
+
+context('Import Product - weight factory feature - empty value 2', () => {
+    it('products - weight factory feature - empty value 2 - new job', () => {
+        //login
+        cy.loginToAdminPanel('ee')
+
+        //go to import page
+        cy.get('.item-import-job').find('a').as('goToImportPageLink')
+        cy.get('@goToImportPageLink').click({force:true})
+
+        //go to new job page
+        cy.get('#add').as('addJobButton')
+        cy.get('@addJobButton').click()
+
+        //specify general section
+        cy.generalImportSectionWithoutReIndex('Product Import - weight factory feature - empty value 2')
+
+        //specify Import Settings section
+        cy.get('.fieldset_settings').find('.fieldset-wrapper-title').as('fieldsetSettings')
+        cy.get('@fieldsetSettings').click()
+        cy.get('.settings_entity').find('select').as('settingsEntity')
+        cy.get('@settingsEntity').select('catalog_product');
+        //change weight factory value
+        cy.get('[data-index="weight_factor"]').find('input').clear()
+
+        //specify Import Behavior section
+        cy.get('.fieldset_behavior').find('.fieldset-wrapper-title').as('fieldsetBehaviour')
+        cy.get('.behavior_behavior').find('select').as('behaviorBehavior')
+        cy.get('@behaviorBehavior').select('append');
+
+        //specify Import Source section
+        cy.fileSource('pub/media/importexport/test/products-convert-weight-to-g.csv')
+
+        //validate Import file
+        cy.get('.source_check_button').click()
+        cy.get('.fieldset_source').contains('File validated successfully',{timeout: 60000})
+
+        //save and run process
+        cy.get('#save_and_run').click({force:true})
+        cy.get('.run').click()
+
+        //check Import results
+        cy.consoleImportResultWithoutReIndex('Entity catalog_product')
+
+        //check that products were created
+        cy.get('#menu-magento-catalog-catalog').find('.item-catalog-products').find('a').as('goToProductsGrid')
+        cy.get('@goToProductsGrid').click({force:true})
+        cy.get('[data-bind="collapsible: {openClass: false, closeOnOuter: false}"]',{timeout: 10000}).find('button').as('filtersButton')
+        cy.get('@filtersButton').click({force:true})
+        cy.get('[name="sku"]').invoke('val', 'convert-weight').trigger('change',{force:true})
+        cy.get('[data-bind="i18n: \'Apply Filters\'"]',{timeout: 10000}).as('applyFiltersButton')
+        cy.get('@applyFiltersButton').click({force:true})
+    
+        //check that weight has value '1'
+        cy.get('.admin__data-grid-outer-wrap').contains('convert-weight-product',{timeout: 10000})
+        cy.get('.admin__data-grid-outer-wrap').contains('convert-weight-product').click({force:true});
+        cy.get('[data-index="weight"]',{timeout: 10000}).find('[name="product[weight]"]').should('have.value','1')
+    })
+})
