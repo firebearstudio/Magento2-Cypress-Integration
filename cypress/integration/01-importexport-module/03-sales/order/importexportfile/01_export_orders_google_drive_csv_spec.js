3c34899b12d863cf0eb07bad98e1c358fdf13bbe
Maria Stoika <marystoikaaa@gmail.com>
2023-08-02 18:18:17 +0300

    new tests for import export files of orders were added

---
+++ cypress/integration/01-importexport-module/03-sales/order/importexportfile/01_export_orders_google_drive_csv_spec.js
@@ -0,0 +1,53 @@
+
+context('Export Orders Google Drive csv 1', () => {
+    it('export - orders - google drive csv - new job', () => {
+        //login
+        cy.loginToAdminPanel('ee')
+
+        //go to import page
+        cy.get('.item-export-job').find('a').as('goToExportPageLink')
+        cy.get('@goToExportPageLink').click({force:true})
+
+        //go to new job page
+        cy.get('#add').as('addJobButton')
+        cy.get('@addJobButton').click()
+
+        //specify general section
+        cy.get('.general_is_active',{timeout: 60000}).find('.admin__actions-switch-label').as('generalIsActive')
+        cy.get('@generalIsActive').click()
+        cy.get('.general_title ').find('input')
+            .type('Orders Export - google drive csv')
+            .should('have.value', 'Orders Export - google drive csv')
+
+        //specify Export Settings section
+        cy.get('.settings_entity').find('select').as('settingsEntity')
+        cy.get('@settingsEntity').select('order')
+
+        //specify Export Behavior section
+        cy.get('.behavior_behavior_field_file_format').find('select').as('fileFormat')
+        cy.get('@fileFormat').select('csv');
+
+        //select the Order Entities: Order, Order Items, Addresses
+        cy.get('#18').click()
+        cy.get('#19').click()
+        cy.get('#20').click()
+
+        //specify Import Source section
+        cy.get('[data-index="export_source_entity"]').find('select').select('googledrive')
+        cy.get('[data-index="export_source_googledrive_file_path"]').find('input')
+            .type('new/order-google-drive-test.csv',{force:true})
+            .should('have.value','new/order-google-drive-test.csv')
+        
+        //upload Google Drive key
+        const filepath = 'files/new1-268508-0814a8f755d1.json'
+        cy.get('.export_source_googledrive_signing_key_file').find('input[type="file"]').attachFile(filepath)
+        cy.get('.file-uploader-filename',{timeout: 10000}).contains('new1-268508-0814a8f755d1.json')
+        
+        //save and run process
+        cy.get('#save_and_run').click({force:true})
+        cy.get('.run').click()
+
+        //check Export results
+        cy.consoleExportResult('Entity order')
+    })
+})
