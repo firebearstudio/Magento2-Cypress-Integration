fee62eab02479ab55b6ec616eeb858a418f3c876
Maria Stoika <marystoikaaa@gmail.com>
2023-09-25 18:52:28 +0300

    new test was added for checking import images from one drive

---
+++ cypress/integration/01-importexport-module/01-catalog/1-product/2-features/04-import-source/01-use-image-import-source/05_import_images_onedrive_source.js
@@ -0,0 +1,61 @@
+
+context('Import Products Images One Drive Source 5', () => {
+    it('import products - images - oneDrive - use image import source', () => {
+        //login
+        cy.loginToAdminPanel('ee')
+
+        //go to import page
+        cy.get('.item-import-job').find('a').as('goToImportPageLink')
+        cy.get('@goToImportPageLink').click({force:true})
+
+        //go to new job page
+        cy.get('#add').as('addJobButton')
+        cy.get('@addJobButton').click()
+
+        //specify general section
+        cy.generalImportSectionWithoutReIndex('Product Import - images - oneDrive - use image import source')
+
+        //specify Import Settings section
+        cy.get('.fieldset_settings').find('.fieldset-wrapper-title').as('fieldsetSettings')
+        cy.get('@fieldsetSettings').click()
+        cy.get('.settings_entity').find('select').as('settingsEntity')
+        cy.get('@settingsEntity').select('catalog_product');
+
+        //specify Import Behavior section
+        cy.get('.fieldset_behavior').find('.fieldset-wrapper-title').as('fieldsetBehaviour')
+        cy.get('.behavior_behavior').find('select').as('behaviorBehavior')
+        cy.get('@behaviorBehavior').select('append');
+
+        //specify Import Source section
+
+        cy.get('.type_file').find('select').as('importSourceType')
+        cy.get('@importSourceType').select('csv',{force:true});
+        cy.get('[data-index="import_source"]').find('[name="import_source"]').select('onedrive')
+        cy.get('[data-index="onedrive_onedrive_file_path"]').find('[name="onedrive_file_path"]')
+            .type('test/test_images_2.csv',{force:true})
+            .should('have.value', 'test/test_images_2.csv') 
+        cy.get('[data-index="onedrive_onedrive_import_images_file_dir"]').find('input').as('imagesFilePath')
+        cy.get('@imagesFilePath').invoke('val','test/').trigger('change',{force:true})
+        cy.get('[data-index="image_import_source"]').find('.admin__actions-switch-label').as('useImoprtImageSource')
+        cy.get('@useImoprtImageSource').click({force:true})
+
+        //validate Import file
+        cy.get('.source_check_button').click()
+        cy.get('.fieldset_source').contains('File validated successfully',{timeout: 20000})
+
+        //save and run process
+        cy.get('#save_and_run').click({force:true})
+        cy.get('.run').click()
+
+        //check Import results
+        cy.consoleImportResultWithoutReIndex('Entity catalog_product')
+        cy.get('#debug-run').contains('Wrong URL/path used for attribute image in rows in rows').should('not.exist')
+        cy.get('#debug-run').contains('Wrong URL/path used for attribute small_image in rows in rows').should('not.exist')
+        cy.get('#debug-run').contains('Wrong URL/path used for attribute thumbnail in rows in rows').should('not.exist')
+        cy.get('#debug-run').contains('Wrong URL/path used for attribute swatch_image in rows in rows').should('not.exist')
+        cy.get('#debug-run').contains('Wrong URL/path used for attribute _media_image in rows in rows').should('not.exist')
+        cy.get('#debug-run').contains('Downloading Image From Source for product sku TST-Conf-Simp-S-Gray',{timeout: 10000})
+        cy.get('#debug-run').contains('Downloading Image From Source for product sku TST-Conf-Simp-S-Green',{timeout: 10000})
+        cy.get('#debug-run').contains('Downloading Image From Source for product sku TST-Conf-Simp-S-Purple',{timeout: 10000})
+    })
+})
