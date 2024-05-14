e3f92fd80e7c5f5b173571c2be3005cad5995087
Maria Stoika <marystoikaaa@gmail.com>
2024-05-14 18:48:40 +0300

    new test was added for import products to specific shared catalogs

---
+++ cypress/integration/03-b2b-module/shared_catalog/18_import_products_with_shared_catalog_spec.js
@@ -0,0 +1,60 @@
+
+context('Import Products With Shared Catalog 18', () => {
+    it('products with shared catalogs - new job', () => {
+        //login
+        cy.loginToAdminPanel('ee')
+
+        //go to import page
+        cy.get('.item-import-job').find('a').as('goToImportPageLink')
+        cy.get('@goToImportPageLink').click({force:true})
+
+        //go to new job page
+        cy.get('#add').as('addJobButton')
+        cy.get('@addJobButton').click({force:true})
+
+        //specify general section
+        cy.generalImportSectionWithoutReIndex('Product Import - with - shared catalog')
+
+        //specify Import Settings section
+        cy.get('.fieldset_settings').find('.fieldset-wrapper-title').as('fieldsetSettings')
+        cy.get('@fieldsetSettings').click({force:true})
+        cy.get('.settings_entity').find('select').as('settingsEntity')
+        cy.get('@settingsEntity').select('catalog_product',{force:true});
+
+        //specify Import Behavior section
+        cy.get('.fieldset_behavior').find('.fieldset-wrapper-title').as('fieldsetBehaviour')
+        cy.get('.behavior_behavior').find('select').as('behaviorBehavior')
+        cy.get('@behaviorBehavior').select('replace',{force:true});
+
+        //specify Import Source section
+        cy.get('.type_file').find('select').as('importSourceType')
+        cy.get('@importSourceType').select('csv',{force:true});
+        cy.fileSource('pub/media/importexport/test/products_with_shared_catalog.csv')
+       
+        //validate Import file
+        cy.get('.source_check_button').click({force:true})
+        cy.get('.fieldset_source').contains('File validated successfully',{timeout: 60000})
+
+        //save and run process
+        cy.get('#save_and_run').click({force:true})
+        cy.get('.run').click()
+
+        //check Import results
+        cy.consoleImportResultWithoutReIndex('Entity catalog_product')
+
+        //check that product was imported to specific shared catalogs
+        cy.get('#menu-magento-catalog-catalog').find('.item-catalog-products').find('a').as('goToProductsGrid')
+        cy.get('@goToProductsGrid').click({force:true})
+        cy.get('[data-bind="collapsible: {openClass: false, closeOnOuter: false}"]',{timeout: 10000}).find('button').as('filtersButton')
+        cy.get('@filtersButton').click({force:true})
+        cy.get('[name="sku"]').invoke('val', 'TST-Conf-Simp-S-Gra').trigger('change',{force:true})
+        cy.get('[data-bind="i18n: \'Apply Filters\'"]',{timeout: 10000}).as('applyFiltersButton')
+        cy.get('@applyFiltersButton').click({force:true})
+        cy.get('.admin__data-grid-outer-wrap').contains('TST-Conf-Simp-S-Gray',{timeout: 10000})
+        cy.get('.admin__data-grid-outer-wrap').contains('TST-Conf-Simp-S-Gray').click({force:true});
+        cy.get('[data-index="shared_catalog"]',{timeout: 20000}).find('.fieldset-wrapper-title').click({force:true})
+        cy.get('[name="product[shared_catalog]"]').contains('Default (General)')
+        cy.get('[name="product[shared_catalog]"]').contains('Shared Catalog 3')
+
+    })
+})
